30b99b6be2d07237035d577f6b59204247592552
Pradnya Patil <pradnyap8767@gmail.com>
2025-06-10 14:20:00 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Headers from "./Components/Headers/Headers";
 
-import HomePage from './pages/HomePage';
+
 import AboutPage from './pages/AboutPage';
 import CoursesPage from './pages/CoursesPage';
 import ContactPage from './pages/ContactPage';
 import AdmissionPage from './pages/AdmissionPage';
 import NotFoundPage from './pages/NotFoundPage';
+import HomePage from "./pages/HomePage";
 
 
 
