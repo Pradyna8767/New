79fe53b6df9dc3a48280b5ba7b0a1159f73b831c
Pradnya Patil <pradnyap8767@gmail.com>
2025-06-11 09:44:02 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import CoursesPage from "./pages/CoursesPage";
+import ContactPage from "./pages/ContactPage";
+import AdmissionPage from "./pages/AdmissionPage";
+
 
 
 
@@ -12,8 +17,12 @@ const App = () => {
     <Router>
         
           <Routes>
-             <Route path="/" element={<HomePage/>} /> 
-           
+            <Route path="/" element={<HomePage />} />
+            <Route path="/aboutpage" element={<AboutPage />} />
+            <Route path="/coursespage" element={<CoursesPage/>} />
+            <Route path="/contactpage" element={<ContactPage/>} />
+            <Route path="/admissionpage" element={<AdmissionPage/>} />
+            <Route path="/aboutpage" element={<AboutPage/>} />
           </Routes>
         </Router>
   )
