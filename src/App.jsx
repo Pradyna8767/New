64684c8224442baf008639fe5e33d043e9a0ab3e
Pradnya Patil <pradnyap8767@gmail.com>
2025-06-11 09:18:49 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import React from "react";
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-
-
-import CoursesPage from './pages/CoursesPage';
-import ContactPage from './pages/ContactPage';
-import AdmissionPage from './pages/AdmissionPage';
-import NotFoundPage from './pages/NotFoundPage';
 import HomePage from "./pages/HomePage";
-import AboutPage from "./Pages/AboutPage";
+
 
 
 
